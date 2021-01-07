1d324e30c39f3efd7de2beafa99825980df3e47b
Brittany Feenstra <brittany.feenstra@formidable.com>
2021-01-07 14:36:05 -0700

    adding exports for typeahead and menu

--- assets/src/design-system/components/index.js
+++ assets/src/design-system/components/index.js
@@ -17,9 +17,11 @@
 export * from './button';
 export { Dialog } from './dialog';
 export { DropDown } from './dropDown';
+export * from './menu';
 export { Modal } from './modal';
 export { Pill } from './pill';
 export { Popup, PLACEMENT } from './popup';
 export * as Snackbar from './snackbar';
 export * from './tooltip';
+export { Typeahead } from './typeahead';
 export { Text, Display, Headline } from './typography';
